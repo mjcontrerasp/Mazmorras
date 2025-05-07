89b17d01ff9ddcc96a33ae02b5c6a38204156a5d
gloriajean02 <gcuradog01@iesalbarregas.es>
2025-05-07 14:09:37 +0200

    introduction view design

--- Mazmorras/src/main/java/com/achos/controllers/IntroductionController.java
+++ Mazmorras/src/main/java/com/achos/controllers/IntroductionController.java
@@ -6,16 +6,77 @@ import com.achos.SceneID;
 import com.achos.SceneManager;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.SplitPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 public class IntroductionController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
+    StackPane mainStackPane;
+
+    @FXML
+    ImageView background;
+
+    @FXML
+    VBox mainVBox;
+
+    @FXML
+    StackPane stackPersonajes;
+
+    @FXML
+    ImageView img_inicio;
+
+    @FXML
+    StackPane stackIntro;
+
+    @FXML
+    Pane cajaTexto;
+
+    @FXML
+    VBox texto;
+
+    @FXML
+    Label title;
+
+    @FXML
+    Label p1;
+
+    @FXML
+    Label p2;
+
+    @FXML
+    StackPane stackButton;
+
+    @FXML
+    SplitPane splitButton;
+
+    @FXML
+    StackPane stackCrear;
+
+    @FXML
+    Button crearPersonaje;
+
+    @FXML
+    StackPane stackJugar;
+
+    @FXML
+    Button jugar;
+
+    @FXML
+    Button startButton;
+
+    @FXML
+    private void switchToEdit() throws IOException {
         SceneManager.getInstance().loadScene(SceneID.EDITPLAYER);
     }
 
     @FXML
-    private void switchToTertiary() throws IOException {
+    private void switchToGame() throws IOException {
         SceneManager.getInstance().loadScene(SceneID.TERTIARY);
     }
 }
